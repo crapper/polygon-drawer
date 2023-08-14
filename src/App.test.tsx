82dc26c6c43bede80a5e5eaf3330b38286d3d09d
crapper <crapper.612@gmail.com>
2023-08-14 16:27:30 +0800

    Add Vector, RectElement

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,82 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { RectSpec, getAnchorPos, TOP_LEFT, Vector, BOTTOM_RIGHT, transformRectSpec, RIGHT, createRectSpec, LEFT, TOP, BOTTOM } from "./App";
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+export { }
+
+/*
+
+The following code works in a 2D coordinate system and y axis increases by north and x axis increases by east
+
+*/
+
+test('transformRectSpec', () => {
+  let ans: RectSpec;
+
+  expect(getAnchorPos({ x: 0, y: 0, width: 10, height: 10 }, TOP_LEFT)).toEqual(new Vector(-5, 5));
+  expect(getAnchorPos({ x: 0, y: 0, width: 10, height: 10 }, BOTTOM_RIGHT)).toEqual(new Vector(5, -5));
+
+  ans = transformRectSpec({ x: 0, y: 0, width: 10, height: 10 }, BOTTOM_RIGHT, new Vector(6, 6));
+  expect(ans).toEqual({ x: 0.5, y: 5.5, width: 11, height: 1 });
+
+  ans = transformRectSpec({ x: 5, y: -5, width: 10, height: 10 }, BOTTOM_RIGHT, new Vector(6, 6));
+  expect(ans).toEqual({ x: 3, y: 3, width: 6, height: 6 });
+
+  ans = transformRectSpec({ x: 0, y: 0, width: 10, height: 7 }, RIGHT, new Vector(8, 0));
+  expect(ans).toEqual({ x: 1.5, y: 0, width: 13, height: 7 });
+
+  ans = transformRectSpec({ x: 0, y: 0, width: 10, height: 7 }, RIGHT, new Vector(8, 1));
+  expect(ans).toEqual({ x: 1.5, y: 0, width: 13, height: 7 });
+
+  ans = transformRectSpec(createRectSpec(-5, 5, 5, -5), BOTTOM_RIGHT, new Vector(6, 6));
+  expect(ans).toEqual(createRectSpec(-5, 6, 6, 5));
+
+  ans = transformRectSpec(createRectSpec(0, 0, 10, -10), BOTTOM_RIGHT, new Vector(6, 6));
+  expect(ans).toEqual(createRectSpec(0, 6, 6, 0));
+
+  ans = transformRectSpec(createRectSpec(-5 + 13, 3.5 + 13, 5 + 13, -3.5 + 13), RIGHT, new Vector(8 + 13, 0));
+  expect(ans).toEqual(createRectSpec(-5 + 13, 3.5 + 13, 8 + 13, -3.5 + 13));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), RIGHT, new Vector(8, 0));
+  expect(ans).toEqual(createRectSpec(-5, 3.5, 8, -3.5));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), RIGHT, new Vector(2, 0));
+  expect(ans).toEqual(createRectSpec(-5, 3.5, 2, -3.5));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), RIGHT, new Vector(-6, 0));
+  expect(ans).toEqual(createRectSpec(-6, 3.5, -5, -3.5));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), LEFT, new Vector(-8, 0));
+  expect(ans).toEqual(createRectSpec(-8, 3.5, 5, -3.5));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), LEFT, new Vector(-2, 0));
+  expect(ans).toEqual(createRectSpec(-2, 3.5, 5, -3.5));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), LEFT, new Vector(6, 0));
+  expect(ans).toEqual(createRectSpec(5, 3.5, 6, -3.5));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), TOP, new Vector(0, 8));
+  expect(ans).toEqual(createRectSpec(-5, 8, 5, -3.5));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), TOP, new Vector(0, 2));
+  expect(ans).toEqual(createRectSpec(-5, 2, 5, -3.5));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), TOP, new Vector(0, -6));
+  expect(ans).toEqual(createRectSpec(-5, -3.5, 5, -6));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), BOTTOM, new Vector(0, -8));
+  expect(ans).toEqual(createRectSpec(-5, 3.5, 5, -8));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), BOTTOM, new Vector(0, -2));
+  expect(ans).toEqual(createRectSpec(-5, 3.5, 5, -2));
+
+  ans = transformRectSpec(createRectSpec(-5, 3.5, 5, -3.5), BOTTOM, new Vector(0, 6));
+  expect(ans).toEqual(createRectSpec(-5, 6, 5, 3.5));
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,193 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { get } from 'http';
+import { KonvaEventObject } from 'konva/lib/Node';
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Stage, Layer, Star, Text, Rect, Circle } from 'react-konva';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class Vector {
+  constructor(public x: number, public y: number) { }
+
+  add(other: Vector) {
+    return new Vector(this.x + other.x, this.y + other.y);
+  }
+
+  subtract(other: Vector) {
+    return new Vector(this.x - other.x, this.y - other.y);
+  }
+
+  multiply(other: Vector) {
+    return new Vector(this.x * other.x, this.y * other.y);
+  }
+
+  divide(other: Vector) {
+    return new Vector(this.x / other.x, this.y / other.y);
+  }
+
+  distance(other: Vector) {
+    return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+  }
+
+  setXY(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  clone() {
+    return new Vector(this.x, this.y);
+  }
 }
 
-export default App;
+export interface RectSpec {
+  x: number; // center X
+  y: number; // center Y
+  width: number;
+  height: number;
+}
+
+export interface RectObject {
+  rect: RectSpec;
+  id: string;
+  rotation?: number;
+  isDragging?: boolean;
+}
+
+export interface CircleObject {
+  x: number; // center X
+  y: number; // center Y
+  id: string;
+  radius: number;
+}
+
+// type a = {x: 1, y: 1};
+
+export const TOP_LEFT: Readonly<Vector> = new Vector(-1, 1);
+export const TOP: Readonly<Vector> = new Vector(0, 1);
+export const TOP_RIGHT: Readonly<Vector> = new Vector(1, 1);
+export const RIGHT: Readonly<Vector> = new Vector(1, 0);
+export const BOTTOM_RIGHT: Readonly<Vector> = new Vector(1, -1);
+export const BOTTOM: Readonly<Vector> = new Vector(0, -1);
+export const BOTTOM_LEFT: Readonly<Vector> = new Vector(-1, -1);
+export const LEFT: Readonly<Vector> = new Vector(-1, 0);
+export const ALL_ANCHORS: Readonly<Vector>[] = [TOP_LEFT, TOP, TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM, BOTTOM_LEFT, LEFT];
+
+export enum Anchor {
+  TOP_LEFT,
+  TOP,
+  TOP_RIGHT,
+  RIGHT,
+  BOTTOM_RIGHT,
+  BOTTOM,
+  BOTTOM_LEFT,
+  LEFT
+}
+
+export function getAnchorPos(rect: RectSpec, anchorVec: Vector): Vector { //get specific anchor position
+  return new Vector(rect.x, rect.y).add(anchorVec.multiply(new Vector(rect.width / 2, rect.height / 2)));
+}
+
+export function transformRectSpec(oldRect: RectSpec, oldAnchorVec: Vector, newAnchorPos: Vector): RectSpec {
+  const oldAnchorPos = getAnchorPos(oldRect, oldAnchorVec); // old anchor position
+  const diagonalAnchorPos = getAnchorPos(oldRect, oldAnchorVec.multiply(new Vector(-1, -1))); // get opposite of old anchor position
+  const correctedNewAnchorPos = oldAnchorPos.add(newAnchorPos.subtract(oldAnchorPos).multiply(oldAnchorVec).multiply(oldAnchorVec)); // 1. remove irrelevant axis by some x = 0/ y=0 2. prevent -1 affect anchor position
+  const newRectCenter = correctedNewAnchorPos.add(diagonalAnchorPos).divide(new Vector(2, 2)); //
+  const newRectWidth = oldAnchorVec.x === 0 ? oldRect.width : Math.abs(correctedNewAnchorPos.x - diagonalAnchorPos.x);
+  const newRectHeight = oldAnchorVec.y === 0 ? oldRect.height : Math.abs(correctedNewAnchorPos.y - diagonalAnchorPos.y);
+
+  return {
+    x: newRectCenter.x,
+    y: newRectCenter.y,
+    width: newRectWidth,
+    height: newRectHeight
+  };
+}
+
+export function createRectSpec(topLeftX: number, topLeftY: number, bottomRightX: number, bottomRightY: number): RectSpec {
+  const width = bottomRightX - topLeftX;
+  const height = topLeftY - bottomRightY;
+  return {
+    x: topLeftX + width / 2,
+    y: bottomRightY + height / 2,
+    width,
+    height
+  };
+}
+
+function makeID(length: number) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function generateShapes(): RectObject[] {
+  let rectspec: RectSpec
+  let x = Math.random() * window.innerWidth;
+  let y = Math.random() * window.innerHeight;
+  rectspec = createRectSpec(x, y, x + 50, y + 50)
+  return [{ rect: rectspec, id: makeID(10), rotation: 0, isDragging: true }];
+}
+
+const INITIAL_STATE = generateShapes();
+
+
+export function RectElement(props: { rectObject: RectObject, setSelected: React.Dispatch<React.SetStateAction<string | null>> }) {
+  const { rectObject, setSelected } = props;
+
+  const handleDragMove = (e: KonvaEventObject<DragEvent>) => {
+    const width = rectObject.rect.width;
+    const height = rectObject.rect.height;
+    rectObject.rect = {
+      x: e.target.x() - width / 2,
+      y: e.target.y() - height / 2,
+      width: width,
+      height: height,
+    }
+  }
+
+  return <Rect
+    id={rectObject.id}
+    x={rectObject.rect.x + rectObject.rect.width / 2}
+    y={rectObject.rect.y + rectObject.rect.height / 2}
+    width={rectObject.rect.width}
+    height={rectObject.rect.height}
+    numPoints={6}
+    innerRadius={20}
+    outerRadius={40}
+    fill="#89b717"
+    opacity={0.8}
+    draggable
+    rotation={rectObject.rotation}
+    scaleX={rectObject.isDragging ? 1.2 : 1}
+    scaleY={rectObject.isDragging ? 1.2 : 1}
+    onClick={(e) => { setSelected(rectObject.id) }}
+    onDragMove={(e) => { handleDragMove(e) }}
+  />
+}
+
+
+export default function App() {
+  const [rectObjects, setlst] = React.useState<RectObject[]>(INITIAL_STATE);
+  // const [points, setPoints] = React.useState<CircleObject[]>([]);
+  // const [onCircle, setOnCircle] = React.useState(true);
+  const [selected, setSelected] = React.useState<string | null>(null);
+
+  function findRectObject(id: string) {
+    return rectObjects.find((r) => r.id === id);
+  }
+
+
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text text="Try to drag a star" />
+        {rectObjects.map((star) => (
+          <RectElement key={star.id} rectObject={star} setSelected={setSelected} />
+        ))}
+
+      </Layer>
+    </Stage>
+  );
+};
